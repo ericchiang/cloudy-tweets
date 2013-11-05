ae887ee7f1ba016cbce282c89a0a1a3510f75a0c
ericchiang <eric.chiang.m@gmail.com>
2013-11-05 07:12:16 +0100

    CVs can be run on one feature generation

--- run_classifier.py
+++ run_classifier.py
@@ -15,22 +15,29 @@ def runCV(y,k_fold):
     y_pred = np.array([0.] * len(y))
     i = 0
     for train_indices,test_indices in k_fold:
-        print '.',
-        clf     = Ridge(alpha=1e-5)
-        train_file = open('fold_data/train_%s' % (i,), 'r')
+        clf = Ridge(alpha=1e-7)
+        # Read training data from file
+        train_file = open('fold_data/train_%s.pk' % (i,), 'r')
         X_train = pk.load(train_file)
         train_file.close()
         y_train = y[train_indices]
+        # fit model
         clf.fit(X_train,y_train)
+        # Clear Memory
         del X_train
         del y_train
-        test_file = open('fold_data/test_%s' % (i,), 'r')
+        # Read test data from file
+        test_file = open('fold_data/test_%s.pk' % (i,), 'r')
         X_test  = pk.load(test_file)
+        test_file.close()
+        # Run prediction
         y_pred[test_indices] = clf.predict(X_test)
+        # Clear memory
         del clf
         del X_test
         i += 1
 
+    # Because all scores must be between 0 and 1, augment 
     for i in range(len(y_pred)):
         if y_pred[i] < 0.0:
             y_pred[i] = 0.0
@@ -73,40 +80,51 @@ if __name__ == '__main__':
     headers = data[0]
     data = data[1:]
 
-    # Parse each tweet into a bag of words
-    X_bag_of_words = []
-    for row in data:
-        bag_of_words = parseTweet(row[1])
-        X_bag_of_words.append(bag_of_words)
-    X_bag_of_words = np.array(X_bag_of_words)
+    use_existing_features = True
 
+    if not use_existing_features:
+        # Parse each tweet into a bag of words
+        X_bag_of_words = []
+        for row in data:
+            bag_of_words = parseTweet(row[1])
+            X_bag_of_words.append(bag_of_words)
+        X_bag_of_words = np.array(X_bag_of_words)
+    
+    
+        """
+        Generate feature matrix using the top n words. Each column represents a
+        word and each value represents the count of said word for that row.
+        """
+        printInfo("Generating Features")
+        feat_generator = TopWordsFeatures(n=600)
+        X = feat_generator.fit(X_bag_of_words).generateFeatures(X_bag_of_words)
+        del X_bag_of_words
+    
+        printInfo("Features generated, shape: %s" % (X.shape,))
+    
+        num_folds = 10
+        printInfo("Saving folds to disk (%s folds)" % (num_folds,))
+        k_fold = KFold(n=X.shape[0],n_folds=num_folds,indices=True)
+        i = 0
+        for train_indices,test_indices in k_fold:
+            printInfo("  Processing fold %s" % (i))
+            train_file = open('fold_data/train_%s.pk' % (i,),'w')
+            pk.dump(X[train_indices,:],train_file)
+            train_file.close()
+            test_file = open('fold_data/test_%s.pk' % (i,),'w')
+            pk.dump(X[test_indices,:],test_file)
+            test_file.close()
+            i += 1
+    
+        del X
+        folds_file = open('fold_data/k_fold.pk','w')
+        pk.dump(k_fold,folds_file)
+        folds_file.close()
+    else:
+        folds_file = open('fold_data/k_fold.pk','r')
+        k_fold = pk.load(folds_file)
+        folds_file.close()
 
-    """
-    Generate feature matrix using the top n words. Each column represents a
-    word and each value represents the count of said word for that row.
-    """
-    printInfo("Generating Features")
-    feat_generator = TopWordsFeatures(n=300)
-    X = feat_generator.fit(X_bag_of_words).generateFeatures(X_bag_of_words)
-    del X_bag_of_words
-
-    printInfo("Features generated, shape: %s" % (X.shape,))
-
-    num_folds = 10
-    printInfo("Saving folds to disk (%s folds)" % (num_folds,))
-    k_fold = KFold(n=X.shape[0],n_folds=num_folds,indices=True)
-    i = 0
-    for train_indices,test_indices in k_fold:
-        printInfo("  Processing fold %s" % (i))
-        train_file = open('fold_data/train_%s' % (i,),'w')
-        pk.dump(X[train_indices,:],train_file)
-        train_file.close()
-        test_file = open('fold_data/test_%s' % (i,),'w')
-        pk.dump(X[test_indices,:],test_file)
-        test_file.close()
-        i += 1
-
-    del X
 
     """
 
@@ -118,6 +136,8 @@ if __name__ == '__main__':
     X = pca.fit_transform(X)
     """
     # printInfo("Feature reduced, shape: %s" % (X.shape,))
+
+    printInfo("Running cross validation")
 
     mses = []
     for var_name in var_names:
@@ -131,12 +151,12 @@ if __name__ == '__main__':
             y.append(np.float(row[var_index]))
         y = np.array(y)
 
-        printInfo("Running CV for vairable '%s'" % (var_name,))
+        printInfo("  Running CV on vairable '%s'" % (var_name,))
         y_pred = runCV(y,k_fold)
         mse = np.mean((y - y_pred)**2)
         del y_pred
         mses.append(mse)
-        printInfo("Results for variable '%s'" % (var_name,))
-        printInfo("  MSE : %s" % (mse,))
+        printInfo("  Results for variable '%s'" % (var_name,))
+        printInfo("    MSE : %s" % (mse,))
     for mse in mses:
         print mse
