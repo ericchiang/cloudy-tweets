b8c37b5e33a57ad16e0f0c3cf1180a8ca656be46
ericchiang <eric.chiang.m@gmail.com>
2013-11-07 09:12:46 +0100

    Processes rearranged to allow for more features.

--- feature_generation.py
+++ feature_generation.py
@@ -12,89 +12,41 @@ import nltk
 from scipy.sparse import lil_matrix, vstack
 
 """
-Class for generating feature matrices from bag-of-words.
+Feature generator. Creates a sparce feature matrix from bag of words.
+
+Each feature represents the number of times a word apears in a bag. The number
+of features are limited to the top n words.
 """
-class BagOfWordsFeatures(object):
-
-    """
-    Generate training features. Will also 'fit' this class so further feature
-    matrices of an identical format can be created.
-    """
-    def genTrainFeats(self,X_bag_of_words):
-        if self.n != 0:
-            raise Exception("Feature generator already trained")
-        # Generate word counts
-        word_counts = {}
-        for bag_of_words in X_bag_of_words:
-            for word in bag_of_words:
-                try:
-                    word_counts[word] += 1
-                except KeyError:
-                    word_counts[word] = 1
-        for word in word_counts.keys():
-            if word_counts[word] >= self.rare:
-                self.word_indexes[word] = len(self.word_indexes)
-        self.n = len(self.word_indexes)
-        return self.genTestFeats(X_bag_of_words)
-
-    """
-    Generate test features using the same format created by the training
-    features.
-    """
-    def genTestFeats(self,X_bag_of_words):
-        features = None
-        combine_limit = 10000
-        for bag_of_words in X_bag_of_words:
-            X_feats = [0.] * self.n
-            for word in bag_of_words:
-                try:
-                    X_feats[self.word_indexes[word]] += 1.
-                except KeyError:
-                    None
-            features.append(X_feats)
-            if len(features) >= combine_limit:
-                if sparse_features is not None:
-                    sparse_features = vstack([sparse_features,
-                                              lil_matrix(features)])
-                else:
-                    sparse_features = lil_matrix(features)
-                features = []
-        if sparse_features is not None:
-            sparse_features = vstack([sparse_features,lil_matrix(features)])
-        else:
-            sparse_features = lil_matrix(features)
-            features = []
-
-        return lil_matrix(sparse_features)
-
-    def __init__(self,rare=3):
-        # min times a word must appear to be included in feature matrix
-        self.rare = rare
-        # index of word for feature matrix
-        self.word_indexes = {}
-        # number of features
-        self.n = 0
-
 class TopWordsFeatures(object):
+
+    """
+    Generate word counts and determine the top n words. This will fix the
+    definition of the feature space.
+    """
     def fit(self,X_bag_of_words):
         all_words = []
         for bag_of_words in X_bag_of_words:
             all_words.extend(bag_of_words)
-        top_words = nltk.FreqDist(all_words).keys()[:self.n]
+        top_words = nltk.FreqDist(all_words).keys()[:self.m]
         print top_words
         for i in range(len(top_words)):
-            self.word_indexes[top_words[i]] = i
+            self.word_indices[top_words[i]] = i
         return self
 
+    """
+    Generate a sparce matrix representing the feature space of a set of bag
+    of words.
+    """
     def generateFeatures(self,X_bag_of_words):
         features = []
         sparse_features = None
+        # Limits the use of dense matrix before merging into sparce matrix 
         combine_limit = 10000
         for bag_of_words in X_bag_of_words:
-            X_feats = [0.] * self.n
+            X_feats = [0.] * self.m
             for word in bag_of_words:
                 try:
-                    X_feats[self.word_indexes[word]] += 1.
+                    X_feats[self.word_indices[word]] += 1.
                 except KeyError:
                     None
             features.append(X_feats)
@@ -114,6 +66,6 @@ class TopWordsFeatures(object):
         return lil_matrix(sparse_features)
 
 
-    def __init__(self,n=100):
-        self.n = n
-        self.word_indexes = {}
+    def __init__(self,m=100):
+        self.m = m
+        self.word_indices = {}
--- run_classifier.py
+++ run_classifier.py
@@ -18,13 +18,13 @@ from format_data import *
 from utils import *
 
 """
-Run cross validation
+Run cross validation.
 """
 def runCV(y,k_fold):
     y_pred = np.array([0.] * len(y))
     i = 0
     for train_indices,test_indices in k_fold:
-        clf = Ridge(alpha=1e-7)
+        clf = Ridge(alpha=1e-7) # Ridge Regression... really fast
         # Read training data from file
         train_file = open('fold_data/train_%s.pk' % (i,), 'r')
         X_train = pk.load(train_file)
@@ -60,11 +60,15 @@ Print usage
 def usage():
     print """
         Usage:
-        %s [train.csv] [var names... ]
+        %s [train.csv] [num features]
         """ % (sys.argv[0],)
 
+
+"""
+Do everything
+"""
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) != 3:
         usage()
         sys.exit(2)
 
@@ -75,20 +79,20 @@ if __name__ == '__main__':
         sys.stderr.write("[Error] Could not open file '%s'" % (sys.argv[1],))
         sys.exit(2)
 
+    num_feats = int(sys.argv[2])
+
     # Variables cv should be run on
-    if len(sys.argv) > 2:
-        var_names = sys.argv[2:]
-    else:
-        var_names = ['s1','s2','s3','s4','s5',
-                     'w1','w2','w3','w4',
-                     'k1','k2','k3','k4','k5','k6','k7','k8',
-                     'k9','k10','k11','k12','k13','k14','k15']
+    var_names = ['s1','s2','s3','s4','s5',
+                 'w1','w2','w3','w4',
+                 'k1','k2','k3','k4','k5','k6','k7','k8',
+                 'k9','k10','k11','k12','k13','k14','k15']
 
     # Parse data from file
     data = parseTwitterCSV(tweet_csv)
     headers = data[0]
     data = data[1:]
 
+    # Read features from file or generate new ones?
     use_existing_features = False
 
     if not use_existing_features:
@@ -100,35 +104,49 @@ if __name__ == '__main__':
         X_bag_of_words = np.array(X_bag_of_words)
     
     
+        printInfo("Determining features space")
+        feat_generator = TopWordsFeatures(m=num_feats)
+        # Determine the top m words and define the feature space
+        feat_generator.fit(X_bag_of_words)
+
         """
-        Generate feature matrix using the top n words. Each column represents a
-        word and each value represents the count of said word for that row.
+        Generate folds and save them to file. Because spliting a sparce matrix
+        into training and test takes a monstrous amount of time, the bag of 
+        words are split and used to generate training and test sparce matrices
+        directly.
         """
-        printInfo("Generating Features")
-        feat_generator = TopWordsFeatures(n=2000)
-        X = feat_generator.fit(X_bag_of_words).generateFeatures(X_bag_of_words)
-        del X_bag_of_words
-    
-        printInfo("Features generated, shape: %s" % (X.shape,))
-    
         num_folds = 10
         printInfo("Saving folds to disk (%s folds)" % (num_folds,))
-        k_fold = KFold(n=X.shape[0],n_folds=num_folds,indices=True)
+        k_fold = KFold(n=len(X_bag_of_words),n_folds=num_folds,indices=True)
         i = 0
         for train_indices,test_indices in k_fold:
-            printInfo("  Processing fold %s" % (i))
+
+            printInfo("  %s Processing sparce training features" % (i,))
+            train_bag_of_words = X_bag_of_words[train_indices]
+            train_sparce_feats = \
+                    feat_generator.generateFeatures(train_bag_of_words)
+            del train_bag_of_words
             train_file = open('fold_data/train_%s.pk' % (i,),'w')
-            pk.dump(X[train_indices,:],train_file)
+            pk.dump(train_sparce_feats,train_file)
+            del train_sparce_feats
             train_file.close()
+
+            printInfo("  %s Processing sparce test features" % (i,))
+            test_bag_of_words = X_bag_of_words[test_indices]
+            test_sparce_feats = \
+                   feat_generator.generateFeatures(test_bag_of_words)
+            del test_bag_of_words
             test_file = open('fold_data/test_%s.pk' % (i,),'w')
-            pk.dump(X[test_indices,:],test_file)
+            pk.dump(test_sparce_feats,test_file)
+            del test_sparce_feats
             test_file.close()
             i += 1
     
-        del X
         folds_file = open('fold_data/k_fold.pk','w')
         pk.dump(k_fold,folds_file)
         folds_file.close()
+
+    # If folds are already generated then the K folds object must be recalled
     else:
         folds_file = open('fold_data/k_fold.pk','r')
         k_fold = pk.load(folds_file)
@@ -149,23 +167,30 @@ if __name__ == '__main__':
     printInfo("Running cross validation")
 
     mses = []
+    # Run cross validation on each variable
     for var_name in var_names:
         if var_name not in headers:
             printInfo("No variable named '%s'" % (var_name,))
             continue
         var_index = headers.index(var_name)
 
+        # Generate target data
         y = []
         for row in data:
             y.append(np.float(row[var_index]))
         y = np.array(y)
 
+        # Run cross validation
         printInfo("  Running CV on vairable '%s'" % (var_name,))
         y_pred = runCV(y,k_fold)
+
+        # Calculate mean squared error
         mse = np.mean((y - y_pred)**2)
         del y_pred
         mses.append(mse)
         printInfo("  Results for variable '%s'" % (var_name,))
         printInfo("    MSE : %s" % (mse,))
+
+    # Print all mean squared error cleanly (without timestamp)
     for mse in mses:
         print mse
