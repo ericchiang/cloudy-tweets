e4d32ec2866d48f5c16ccd286f23a289b03aef3a
ericchiang <eric.chiang.m@gmail.com>
2013-11-11 15:53:32 +0100

    Trying different classifiers

--- score_test.py
+++ score_test.py
@@ -5,6 +5,8 @@ import numpy as np
 import pickle as pk
 from sklearn.cross_validation import KFold
 from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet
+from sklearn import svm
 from feature_generation import TopWordsFeatures
 from format_data import *
 from utils import *
@@ -33,7 +35,7 @@ if __name__ == '__main__':
         sys.stderr.write("[Error] Could not open file '%s'" % (sys.argv[2],))
         sys.exit(2)
 
-    num_feats = 3000
+    num_feats = 4000
 
     var_names = ['s1','s2','s3','s4','s5',
                  'w1','w2','w3','w4',
@@ -64,7 +66,7 @@ if __name__ == '__main__':
     all_bags = np.concatenate((train_bag_of_words,test_bag_of_words),axis=0)
 
     feat_generator = TopWordsFeatures(m=num_feats)
-    feat_generator.fit(all_bags)
+    feat_generator.fit(all_bags) # Train on all words
     del all_bags
 
     printInfo("Generating training features")
@@ -101,7 +103,9 @@ if __name__ == '__main__':
             y_train.append(np.float(row[var_index]))
         y_train = np.array(y_train)
 
-        clf = Ridge(alpha=1e-7)
+        # clf = Ridge(alpha=1e-4)
+        # clf = ElasticNet(alpha=1e-5);
+        clf = svm.SVR()
 
         """
         train_file = open('fold_data/train_final.pk','r')
@@ -126,15 +130,23 @@ if __name__ == '__main__':
         exp_num_values = X_test.shape[0]
         y_pred[var_name] = clf.predict(X_test)
 
+        greater_than_1 = 0
+        less_than_0 = 0
+
         for i in range(len(y_pred[var_name])):
             if y_pred[var_name][i] < 0.0:
                 y_pred[var_name][i] = 0.0
+                less_than_0 += 1
             elif y_pred[var_name][i] > 1.0:
                 y_pred[var_name][i] = 1.0
+                greater_than_1 += 1
 
         # del X_test
         # del clf
         assert len(y_pred[var_name]) == exp_num_values
+
+        printInfo("    Scores greater than 1: %s" % (greater_than_1,))
+        printInfo("    Scores less than 0: %s" % (less_than_0,))
 
     try:
         out_csv = open(sys.argv[3], 'w')
